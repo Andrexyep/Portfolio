91db3fbd845c839eb62c0d4f7f92ac273e58ec0a
Andrea Ferrauto <andrex.yep@gmail.com>
2022-03-23 23:34:23 +0100

    Update

--- JS/script.js
+++ JS/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Player", "Level Designer", "Game Designer", "Developer"],
+        strings: ["Player", "Level Designer", "Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Player", "Level Designer", "Game Designer", "Developer"],
+        strings: ["Player", "Level Designer", "Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
